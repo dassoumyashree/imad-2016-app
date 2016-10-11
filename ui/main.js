66745ace5b0e2ae433acbb0d901f7551aad0227f
dassoumyashree <das.soumyashree19@gmail.com>
2016-10-12 02:58:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,6 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
 var img = document.getElementById('madi');
-
-
 img.onclick = function() {
-   img.style.marginleft = '100px';
+   img.style.marginLeft = '100px';
 };
