c77881c02de32b198f88d71314b0e4e1b7ff2185
dassoumyashree <das.soumyashree19@gmail.com>
2016-11-26 23:30:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,103 +1,50 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user: 'baglasaurabh',
+    database: 'baglasaurabh',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    
-
-'article-one' : {
-    title : 'Article One | sdas',
-    heading : 'Article One',
-    date: 'oct 8,2016',
-    content: `<p>
-            This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-            </p>
-            <p>
-                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-            </p>
-            <p>
-                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-            </p>`
-    },
-    'article-two' : {title : 'Article Two | sdas',
-    heading : 'Article Two',
-    date: 'oct 10,2016',
-    content: `<p>
-            This is the content of my second article.
-            </p>`},
-    'article-three' : {title : 'Article Three | sdas',
-    heading : 'Article Three',
-    date: 'oct 18,2016',
-    content: `<p>
-            This is the content of my third article.
-            </p>`}
-};
-function createTemplate (data){
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content; 
-    
-var htmlTemplate =`
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width-device-width, initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-        
-    </head>
-    <body>
-        <div class="contain">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <p>
-            ${date}
-        </p>
-        <div>
-            ${content}
-        </div>
-   </div>
-`;
-return htmlTemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
-
-app.get('/:articleName',function(req,res){
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+var pool = new Pool(config);
+app.get('/article-db', function (req, res) {
+  // make a select request
+  // return a response with the results
+  pool.query('SELECT * FROM article',function (err, result) {
+     if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         res.send(JSON.stringify(result));
+     }
+  });
+ });
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('article one requested and will be served here');
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
